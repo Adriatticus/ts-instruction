f684d598131bbca492f15663f21071c7485d7194
adrian-johnson <4corderoj@gmail.com>
2025-04-01 09:54:53 -0400

    added realine-sync for console input through project

---
+++ ts-demos/ts/friends.ts
@@ -0,0 +1,9 @@
+let friends = [
+  { name: "Matatticus", email: "Matt@AtticusLegacy.Com", age: 37, BFF: "Yes" },
+  { name: "Travatticus", email: "Trav@AtticusLegacy.Com", age: 36, BFF: "Yes" },
+  { name: "Ryatticus", email: "Ryan@AtticusLegacy.Com", age: 32, BFF: "Yes" },
+  { name: "Davatticus", email: "Dave@AtticusLegacy.Com", age: 37, BFF: "Yes" },
+  { name: "Loopatticus", email: "Lupe@AtticusLegacy.Com", age: 48, BFF: "Yes" }
+];
+
+friends.forEach(f => console.log(f));
